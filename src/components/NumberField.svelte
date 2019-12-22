251882ce1a0cfff5c602306ae16fce92fa23e20f
swyx <shawnthe1@gmail.com>
2019-12-22 09:59:19 -0500

    input type number on ios

--- src/components/NumberField.svelte
+++ src/components/NumberField.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <label for="{fieldName}">{label}</label>
-<input min="0" type="number" id="{fieldName}" bind:value={$settings[fieldName]} {placeholder} />
+<input min="0" type="number" pattern="\d*" id="{fieldName}" bind:value={$settings[fieldName]} {placeholder} />
 <div class="form-control-extras">
   <button type="button" on:click={()=> $settings[fieldName] = undefined}> Reset </button>
   <slot> </slot>
